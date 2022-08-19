13af03b650b90b878ebee35dcb638b759d61cc07
chrisislearning <chris.achinga@student.moringaschool.com>
2022-08-19 10:56:22 +0300

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+// Returns a year the KC Chiefs won the superbowl and undefined if the record has no win objects
+let superbowlWin = (superbowlWinRecord) => {
+  let superbowlWon = superbowlWinRecord.find((superbowl) => {
+    return superbowl.result === 'W'
+  })
+  if (superbowlWon) {
+    return superbowlWon.year
+  } else {
+    return undefined
+  }
+}
